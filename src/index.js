148b81122057a07388563142a6eac03f41afb394
Zemmouri Farouk <faroukwebdesign@gmail.com>
2023-08-27 03:52:00 +0100

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const inputDescription = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+  form.addEventListener("submit", function(e){
+    event.preventDefault();
+    const taskDescription = inputDescription.value;
+    const newTask = document.createElement("li");
+    newTask.textContent = taskDescription;
+    taskList.appendChild(newTask);
+    inputDescription.value = "";
+    const deleteTask = document.createElement("button");
+    deleteTask.textContent = "Delete Task";
+    deleteTask.addEventListener("click", function(){
+      taskList.removeChild(newTask);
+    });
+    newTask.appendChild(deleteTask);
+    taskList.appendChild(newTask);
+    taskDescription.value = "";
+  });
 });
